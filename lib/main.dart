b9ac439acd5b8853fb5bb9643e519e17e32cb09c
Abdul Muhsin <muhasink444@gmail.com>
2023-08-08 02:07:01 +0530

    payment gateway issue commit (#2)
    Co-authored-by: a2en <ameenahsan619@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,235 +1,147 @@
-
-import 'package:flutter/material.dart';
-import 'package:payu_checkoutpro_flutter_example/HashService.dart';
-import 'package:payu_checkoutpro_flutter/payu_checkoutpro_flutter.dart';
-import 'package:payu_checkoutpro_flutter/PayUConstantKeys.dart';
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> implements PayUCheckoutProProtocol {
-
-  late PayUCheckoutProFlutter _checkoutPro;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkoutPro = PayUCheckoutProFlutter(this);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PayU Checkout Pro'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text("Start Payment"),
-            onPressed: () async {
-              _checkoutPro.openCheckoutScreen(
-                payUPaymentParams: PayUParams.createPayUPaymentParams(),
-                payUCheckoutProConfig: PayUParams.createPayUConfigParams(),
-              );
-            },
-          ),
-        ),
+      title: 'PayUmoney CheckoutPro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWebView(),
+    );
+  }
+}
+
+class MyWebView extends StatefulWidget {
+  @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  final String url = 'https://secure.payu.in/_payment';
+
+  String key = 'Rd3iUs';
+  String txnid = 'PQI6eeMquYrjEefU';
+  String amount = '10.00';
+  String productinfo = 'iPhone';
+  String firstname = 'PayU User';
+  String email = 'test@gmail.com';
+  String udf1 = '';
+  String udf2 = '';
+  String udf3 = '';
+  String udf4 = '';
+  String udf5 = '';
+  String salt = 'IKtjXMKG';
+
+  late final Map<String, String> postData = {
+    'key': key,
+    'txnid': txnid,
+    'amount': amount,
+    'firstname': firstname,
+    'email': email,
+    'phone': '9876543210',
+    'productinfo': productinfo,
+    'surl': 'https://apiplayground-response.herokuapp.com/',
+    'furl': 'https://apiplayground-response.herokuapp.com/',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    addHashToPostData();
+    Timer(const Duration(seconds: 2), () => postUrl());
+  }
+
+  late final WebViewController _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {
+
+        },
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          return NavigationDecision.navigate;
+        },
+      ),
+    );
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PayU Payment'),
+      ),
+      body: WebViewWidget(
+        controller: _controller,
       ),
     );
   }
 
-  showAlertDialog(BuildContext context, String title, String content) {
-    Widget okButton = TextButton(
-      child: const Text("OK"),
-      onPressed: () {
-        Navigator.pop(context);
-      },
-    );
+  void postUrl() async {
+    // Convert the POST data to a string
+    String postDataString = '';
+    postData.forEach((key, value) {
+      postDataString += '$key=$value&';
+    });
 
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(title),
-            content: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: new Text(content),
-            ),
-            actions: [okButton],
-          );
-        });
+    // Remove the trailing '&' character
+    postDataString = postDataString.substring(0, postDataString.length - 1);
+
+    var code = '''
+     var form = document.createElement('form');
+form.method = 'POST';
+form.action = '$url';
+var postDataString = '$postDataString';
+postDataString.split('&').forEach(function(param) {
+  var input = document.createElement('input');
+  input.type = 'hidden';
+  input.name = param.split('=')[0];
+  input.value = param.split('=')[1];
+  form.appendChild(input);
+});
+document.body.appendChild(form);
+form.submit();
+    ''';
+
+    print(code);
+
+    // Execute the JavaScript code to submit the POST request
+    _controller.runJavaScript(code);
   }
 
-  @override
-  generateHash(Map response) {
-    // Pass response param to your backend server
-    // Backend will generate the hash and will callback to
-    Map hashResponse = HashService.generateHash(response);
-    _checkoutPro.hashGenerated(hash: hashResponse);
-  }
+  void addHashToPostData() {
+    // Concatenate the parameters with the pipe character
+    String hashString = [key, txnid, amount, productinfo, firstname, email, udf1, udf2, udf3, udf4, udf5, '', '', '', '', '', salt].join('|');
 
-  @override
-  onPaymentSuccess(dynamic response) {
-    showAlertDialog(context, "onPaymentSuccess", response.toString());
-  }
-
-  @override
-  onPaymentFailure(dynamic response) {
-    showAlertDialog(context, "onPaymentFailure", response.toString());
-  }
-
-  @override
-  onPaymentCancel(Map? response) {
-    showAlertDialog(context, "onPaymentCancel", response.toString());
-  }
-
-  @override
-  onError(Map? response) {
-    showAlertDialog(context, "onError", response.toString());
-  }
-}
-
-class PayUTestCredentials {
-  static const merchantKey = "smsplus";//TODO: Add Merchant Key
-  //Use your success and fail URL's.
-
-  static const iosSurl = "https://payu.herokuapp.com/ios_success";//TODO: Add Success URL.
-  static const iosFurl = "https://payu.herokuapp.com/ios_failure";//TODO Add Fail URL.
-  static const androidSurl = "https://payu.herokuapp.com/success";//TODO: Add Success URL.
-  static const androidFurl = "https://payu.herokuapp.com/failure";//TODO Add Fail URL.
+    print(hashString);
 
 
-  static const merchantAccessKey = "";//TODO: Add Merchant Access Key - Optional
-  static const sodexoSourceId = ""; //TODO: Add sodexo Source Id - Optional
-}
+    // Generate the SHA-512 hash
+    var bytes = utf8.encode(hashString);
+    var digest = sha512.convert(bytes);
+    String hash = digest.toString();
 
-//Pass these values from your app to SDK, this data is only for test purpose
-class PayUParams {
-  static Map createPayUPaymentParams() {
-    var siParams = {
-      PayUSIParamsKeys.isFreeTrial: true,
-      PayUSIParamsKeys.billingAmount: '1',              //Required
-      PayUSIParamsKeys.billingInterval: 1,              //Required
-      PayUSIParamsKeys.paymentStartDate: '2023-04-20',  //Required
-      PayUSIParamsKeys.paymentEndDate: '2023-04-30',    //Required
-      PayUSIParamsKeys.billingCycle:                    //Required
-      'daily', //Can be any of 'daily','weekly','yearly','adhoc','once','monthly'
-      PayUSIParamsKeys.remarks: 'Test SI transaction',
-      PayUSIParamsKeys.billingCurrency: 'INR',
-      PayUSIParamsKeys.billingLimit: 'ON', //ON, BEFORE, AFTER
-      PayUSIParamsKeys.billingRule: 'MAX', //MAX, EXACT
-    };
-
-    var additionalParam = {
-      PayUAdditionalParamKeys.udf1: "udf1",
-      PayUAdditionalParamKeys.udf2: "udf2",
-      PayUAdditionalParamKeys.udf3: "udf3",
-      PayUAdditionalParamKeys.udf4: "udf4",
-      PayUAdditionalParamKeys.udf5: "udf5",
-      PayUAdditionalParamKeys.merchantAccessKey:
-      PayUTestCredentials.merchantAccessKey,
-      PayUAdditionalParamKeys.sourceId:PayUTestCredentials.sodexoSourceId,
-    };
-
-
-    var spitPaymentDetails =
-    {
-      "type": "absolute",
-      "splitInfo": {
-        PayUTestCredentials.merchantKey: {
-          "aggregatorSubTxnId": "1234567540099887766650092", //unique for each transaction
-          "aggregatorSubAmt": "1"
-        },
-        /* "qOoYIv": {
-          "aggregatorSubTxnId": "12345678",
-          "aggregatorSubAmt": "40"
-       },*/
-      }
-    };
-
-
-    var payUPaymentParams = {
-      PayUPaymentParamKey.key: PayUTestCredentials.merchantKey,
-      PayUPaymentParamKey.amount: "1",
-      PayUPaymentParamKey.productInfo: "Info",
-      PayUPaymentParamKey.firstName: "Abc",
-      PayUPaymentParamKey.email: "test@gmail.com",
-      PayUPaymentParamKey.phone: "9999999999",
-      PayUPaymentParamKey.ios_surl: PayUTestCredentials.iosSurl,
-      PayUPaymentParamKey.ios_furl: PayUTestCredentials.iosFurl,
-      PayUPaymentParamKey.android_surl: PayUTestCredentials.androidSurl,
-      PayUPaymentParamKey.android_furl: PayUTestCredentials.androidFurl,
-      PayUPaymentParamKey.environment: "0", //0 => Production 1 => Test
-      PayUPaymentParamKey.userCredential: null, //TODO: Pass user credential to fetch saved cards => A:B - Optional
-      PayUPaymentParamKey.transactionId:
-      DateTime.now().millisecondsSinceEpoch.toString(),
-      PayUPaymentParamKey.additionalParam: additionalParam,
-      PayUPaymentParamKey.enableNativeOTP: true,
-      // PayUPaymentParamKey.splitPaymentDetails:json.encode(spitPaymentDetails),
-      PayUPaymentParamKey.userToken:"", //TODO: Pass a unique token to fetch offers. - Optional
-    };
-
-    return payUPaymentParams;
-  }
-
-  static Map createPayUConfigParams() {
-    var paymentModesOrder = [
-      {"Wallets": "PHONEPE"},
-      {"UPI": "TEZ"},
-      {"Wallets": ""},
-      {"EMI": ""},
-      {"NetBanking": ""},
-    ];
-
-    var cartDetails = [
-      {"GST": "5%"},
-      {"Delivery Date": "25 Dec"},
-      {"Status": "In Progress"}
-    ];
-    var enforcePaymentList = [
-      {"payment_type": "CARD", "enforce_ibiboCode": "UTIBENCC"},
-    ];
-
-    var customNotes = [
-      {
-        "custom_note": "Its Common custom note for testing purpose",
-        "custom_note_category": [PayUPaymentTypeKeys.emi,PayUPaymentTypeKeys.card]
-      },
-      {
-        "custom_note": "Payment options custom note",
-        "custom_note_category": null
-      }
-    ];
-
-    var payUCheckoutProConfig = {
-      PayUCheckoutProConfigKeys.primaryColor: "#4994EC",
-      PayUCheckoutProConfigKeys.secondaryColor: "#FFFFFF",
-      PayUCheckoutProConfigKeys.merchantName: "PayU",
-      PayUCheckoutProConfigKeys.merchantLogo: "logo",
-      PayUCheckoutProConfigKeys.showExitConfirmationOnCheckoutScreen: true,
-      PayUCheckoutProConfigKeys.showExitConfirmationOnPaymentScreen: true,
-      PayUCheckoutProConfigKeys.cartDetails: cartDetails,
-      PayUCheckoutProConfigKeys.paymentModesOrder: paymentModesOrder,
-      PayUCheckoutProConfigKeys.merchantResponseTimeout: 30000,
-      PayUCheckoutProConfigKeys.customNotes: customNotes,
-      PayUCheckoutProConfigKeys.autoSelectOtp: true,
-      // PayUCheckoutProConfigKeys.enforcePaymentList: enforcePaymentList,
-      PayUCheckoutProConfigKeys.waitingTime: 30000,
-      PayUCheckoutProConfigKeys.autoApprove: true,
-      PayUCheckoutProConfigKeys.merchantSMSPermission: true,
-      PayUCheckoutProConfigKeys.showCbToolbar: true,
-    };
-    return payUCheckoutProConfig;
+    // Add the hash to the POST data
+    postData['hash'] = hash;
   }
 }
